d6a160b09b4433a803a004443a4df22126173601
omid astaraki <omidocean@gmail.com>
2021-09-25 21:27:39 +0330

    first commit

---
+++ override.js
@@ -0,0 +1,14 @@
+// setTimeout(() => {
+//   const observer = new MutationObserver(function (mutations, observer) {
+//     const textElements = document.querySelectorAll(".global-rtl");
+//     console.log(textElements);
+//     textElements.forEach((element) => {
+//       element.setAttribute("dir", "auto");
+//     });
+//   });
+
+//   observer.observe(document, {
+//     subtree: true,
+//     attributes: true,
+//   });
+// }, 10000);
